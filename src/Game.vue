d7a4f2e2e4c6f626187e868d33ef0289ce84ecc1
Opemipo Aikomo <opemipoaikomo@gmail.com>
2019-09-15 18:28:19 +0100

    Initialize Vue PWA

---
+++ src/Game.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="game">
+    <home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+
+export default {
+  name: 'game',
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
